365eca3dd69dc6a9399e823c8d20cfdb544a0acb
Athul Menon <52874407+Athul17x@users.noreply.github.com>
2019-11-19 00:24:10 +0530

    Update Panthera.java

--- Panthera.java
+++ Panthera.java
@@ -3,8 +3,8 @@ public class Panthera implements AnimalKingdom  {
 	String Subclass;
     String CommonName;
 	String habitat;
-    double lifeSpan;
-	double populationStatus;
+    String lifeSpan;
+	String populationStatus;
 	String IUCNstatus;
 	
 	public Panthera(){
