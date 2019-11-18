df5e887e0166c01d5602f686e0c057e6dd26d91d
Caster12 <57310589+Caster12@users.noreply.github.com>
2019-11-18 23:30:05 +0530

    Update Panthera.java

--- Panthera.java
+++ Panthera.java
@@ -1,27 +1,48 @@
 
 public class Panthera implements AnimalKingdom  {
 	static String Genus="Panthera";
-	String Subclass="";
-	String habitat="";
-    double lifeSpan = 0;
-	double populationStatus = 0;
-	String IUCNstatus="";
+	String Subclass;
+    String CommonName;
+	String habitat;
+    double lifeSpan;
+	double populationStatus;
+	String IUCNstatus;
+	
+	  public Panthera()
+	    {
+	        Genus="Canis";
+	        Subclass="default";
+	        CommonName="default";
+	        habitat="default";
+	        lifeSpan = 0;
+	        populationStatus = 0;
+	        IUCNstatus="default";
+	    } 
 
- public Panthera(String s1, String s2, double d1, double d2 , String s3) {
-		super();
+ public Panthera(String s1 , String s2, String s3, double d1, double d2 , String s4) {
 		Subclass=s1;
-		habitat=s2;
+		CommonName=s2;
+		habitat=s3;
 	    lifeSpan = d1;
 		populationStatus = d2;
-		IUCNstatus = s3;
+		IUCNstatus = s4;
 	}
 
 	public void Display() {
 		System.out.println("Scientific Name:"+Genus+" "+Subclass);
+		System.out.println("Common Name:"+CommonName);
 		System.out.println("Habitat:"+habitat);
 		System.out.println("Life Span:"+lifeSpan);
 		System.out.println("Population Status:"+populationStatus);
 		System.out.println("IUCN Status:"+IUCNstatus);
 
 	}
+	public void EditDetails(String s1, String s2, String s3, double d1, double d2 , String s4) {
+		Subclass = s1;
+		CommonName=s2;
+	    habitat = s3;
+		lifeSpan = d1;
+		populationStatus = d2;
+		IUCNstatus = s4;
+		}
 }
